ab5c4bbf35d5c0c5195e3242efe2413a3552d36e
MADiazV1 <diazvalenciam9@gmail.com>
2024-05-16 17:39:28 -0500

    Funcion de cambio de colores de dias restantes de certificados agregada

--- public/scripts/workers.js
+++ public/scripts/workers.js
@@ -1,12 +1,30 @@
 import { expiryTime } from "./dateCalculate.js";
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', () => {
     const timeElements = document.querySelectorAll('[id^="time"]');
-    timeElements.forEach(function(element) {
+    timeElements.forEach((element) => {
         const expiryElement = element.parentElement.querySelector('#exp');
         const expiryText = expiryElement.textContent;
         const expiryDate = new Date(expiryText);
         const daysRemaining = expiryTime(expiryDate);
-        element.innerHTML += `<p>${daysRemaining} días restantes</p>`;
+        element.innerHTML += `<p id="days">${daysRemaining} días restantes</p>`;
+    });
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const timeElements = document.querySelectorAll('[id^="time"]');
+    timeElements.forEach((element) => {
+        const days = element.parentElement.querySelector('#days').textContent;
+        const numberDays = parseInt(days.match(/-?\d+/)[0], 10);
+        const negativeDays = element.parentElement.querySelector('#days');
+
+        if(numberDays >= 60) {
+            element.classList.add('btn-verde');
+        }else if(numberDays < 200 && numberDays > 0) {
+            element.classList.add('btn-amarillo');
+        }else {
+            element.classList.add('btn-rojo');
+            negativeDays.textContent = "No queda tiempo";
+        }
     });
 });
