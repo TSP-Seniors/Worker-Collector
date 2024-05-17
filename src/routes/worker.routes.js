526363858d96c9f0a15f087e83d043464eeb73f5
JPZU <juanpablozp@icloud.com>
2024-05-17 02:26:56 -0500

    PMV listo, corregir URLs

--- src/routes/worker.routes.js
+++ src/routes/worker.routes.js
@@ -3,7 +3,7 @@ import { homePage, workerPage } from "../controllers/worker.controller.js";
 
 const router = Router();
 
-router.get('/', homePage);
-router.get('/workers/:id', workerPage);
+router.get("/", homePage);
+router.get("/workers", workerPage);
 
 export default router;
